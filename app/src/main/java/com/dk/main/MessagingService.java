e6d1950e47cadbbcabd7f50f7b62195e16ec7d24
stanley.li <as881028@gmail.com>
2017-07-21 16:38:39 +0800

    FCM build version setting

--- app/src/main/java/com/dk/main/MessagingService.java
+++ app/src/main/java/com/dk/main/MessagingService.java
@@ -12,8 +12,8 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MessagingService extends FirebaseMessagingService{
-//    @Override
-//    public void onMessageReceived(RemoteMessage remoteMessage) {
-//        Log.d("FCM", "onMessageReceived:"+remoteMessage.getFrom());
-//    }
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d("FCM", "onMessageReceived:"+remoteMessage.getFrom());
+    }
 }
