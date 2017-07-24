479ddc2733212e4f294c4005cc545e6adad78af9
Jack <a0989892835@gmail.com>
2017-07-24 21:34:26 +0800

    新增個人資料

---
+++ app/src/main/java/com/dk/main/Personal_information.java
@@ -0,0 +1,43 @@
+package com.dk.main;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Personal_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+            //取代app_bar的include ， 傳Layout
+        replaceInclude(R.layout.activity_personal_information);
+        initMenuBar();
+    }
+
+    private void replaceInclude(int layout) {
+        View include = findViewById(R.id.include_menu);
+        ViewGroup parent = (ViewGroup) include.getParent();
+        int index = parent.indexOfChild(include);
+        parent.removeView(include);
+        include = getLayoutInflater().inflate(layout, parent, false);
+        parent.addView(include, index);
+    }
+    private void initMenuBar() {
+        //menu tool bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+}
