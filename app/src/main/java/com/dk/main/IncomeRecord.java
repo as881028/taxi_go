febabb630874dbbb36a37ec03f2cf312aa0b83ab
Jack <a0989892835@gmail.com>
2017-08-02 13:11:15 +0800

    收入紀錄，修改個人資料，服務中心

--- app/src/main/java/com/dk/main/IncomeRecord.java
+++ app/src/main/java/com/dk/main/IncomeRecord.java
@@ -1,5 +1,6 @@
 package com.dk.main;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,15 +8,71 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class IncomeRecord extends BaseActivity {
+    ListView income_record_Listview;
+    String[] dates= new String[] {"2017年5月12","2017年5月13日","2017年5月14日","2017年5月15日","2017年5月16日",
+            "2017年5月17日","2017年5月18日","2017年5月19日","2017年5月20日","2017年5月21日"};
+    String[] digitals= {"85","87","65","66","87", "88","89","90","81","82"};
+    String[] monthes= {"一","二","三","四","五", "六","七","八","九","十"};
+    String[] moneies= {"1000NT","1001NT","1002NT","1003NT","1004NT",
+            "1005NT","1006NT","靜宜","東海","逢甲"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setMenuLayout(R.layout.activity_income_record);
+        income_record_Listview = (ListView)findViewById(R.id.income_record_Listview);
+        //建立自訂的Adapter
+        MyAdapter adapter=new MyAdapter(this);
+        //設定ListView 的資源來源
+        income_record_Listview.setAdapter(adapter);
+    }
+    public class MyAdapter extends BaseAdapter {
+        private LayoutInflater myInflater;
+        public MyAdapter(Context c){
+            myInflater = LayoutInflater.from(c);
+        }
+        @Override
+        public int getCount(){
+            return dates.length;
+        }
+        @Override
+        public Object getItem(int position){
+            return dates[position];
+        }
+        public long getItemId(int position){
+            return position;
+        }
+        @Override
+        public View getView (int postition,View convertView, ViewGroup parent){
+            convertView = myInflater.inflate(R.layout.incomerecord_listview_layout,null);
+            //取得XML內容
+            TextView date = ((TextView)
+                    convertView.findViewById(R.id.date));
+            TextView score = ((TextView)
+                    convertView.findViewById(R.id.score));
+            TextView digital = ((TextView)
+                    convertView.findViewById(R.id.digital));
+            TextView month = ((TextView)
+                    convertView.findViewById(R.id.month));
+            TextView month_textView = ((TextView)
+                    convertView.findViewById(R.id.month_textView));
+            TextView money = ((TextView)
+                    convertView.findViewById(R.id.money));
+
+            date.setText(dates[postition]);
+            digital.setText(digitals[postition]);
+            month.setText(monthes[postition]);
+            money.setText(moneies[postition]);
+            return convertView;
+        }
     }
 
 }
--- app/src/main/java/com/dk/main/Opinion.java
+++ app/src/main/java/com/dk/main/Opinion.java
@@ -1,5 +1,7 @@
 package com.dk.main;
 
+import android.content.Context;
+import android.media.Rating;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,15 +9,72 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class Opinion extends MenuBaseActivity {
+
+    ListView opinion_Listview;
+    String[] dates= new String[] {"2017年5月12","2017年5月13日","2017年5月14日","2017年5月15日","2017年5月16日",
+            "2017年5月17日","2017年5月18日","2017年5月19日","2017年5月20日","2017年5月21日"};
+    String[] stars= {"3","2","5","1","1", "2","5","4","4","3"};
+    String[] contents= {"台北火車站","台南火車站","台中火車站","台東火車站","永康火車站",
+            "新市火車站","善化火車站","大橋火車站","新營火車站","柳營火車站"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setMenuLayout(R.layout.activity_opinion);
+        opinion_Listview = (ListView) findViewById(R.id.opinion_Listview);
+        //建立自訂的Adapter
+        Opinion.MyAdapter adapter = new Opinion.MyAdapter(this);
+        //設定ListView 的資源來源
+        opinion_Listview.setAdapter(adapter);
+    }
+    public class MyAdapter extends BaseAdapter {
+        private LayoutInflater myInflater;
+
+        public MyAdapter(Context c) {
+            myInflater = LayoutInflater.from(c);
+        }
+
+        @Override
+        public int getCount() {
+            return dates.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return dates[position];
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int postition, View convertView, ViewGroup parent) {
+            convertView = myInflater.inflate(R.layout.reservation_listview_layout, null);
+            //取得XML內容
+            TextView date_textView = ((TextView)
+                    convertView.findViewById(R.id.date_textView));
+            TextView score = ((TextView)
+                    convertView.findViewById(R.id.score));
+            RatingBar ratingBar = ((RatingBar)
+                    convertView.findViewById(R.id.ratingBar));
+            TextView opinion_content = ((TextView)
+                    convertView.findViewById(R.id.opinion_content));
+
+            date_textView.setText(dates[postition]);
+            //ratingBar.setRating(stars[postition]);
+            opinion_content.setText(contents[postition]);
+            return convertView;
+        }
     }
 
 
